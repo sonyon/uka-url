58ec130547a2b111f7a7c745537081fbb4596616
sonyon <sonyon@gmail.com>
2015-04-18 12:04:06 +0200

    Update ukaurl.sql

--- app/Database/ukaurl.sql
+++ app/Database/ukaurl.sql
@@ -2,11 +2,13 @@
 -- Table structure for `urls`
 -- ----------------------------
 DROP TABLE IF EXISTS `urls`;
-CREATE TABLE `urls` (
-  `id` char(6) COLLATE utf8_unicode_ci NOT NULL,
-  `original` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `urls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `original` text COLLATE utf8_unicode_ci NOT NULL,
+  `short` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `counter` int(6) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `short` (`short`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
